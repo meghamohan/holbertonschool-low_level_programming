57aa89b790526b87f643680c68790cf3d431170e
meghamohan <meghamohan1989@gmail.com>
2017-02-08 22:41:52 +0000

    adding task3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 /*
 *Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
 *Use the function puts
 *You are not allowed to use printf
 *Your program should end with the value 0
 */
-#include<stdio.h>
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
