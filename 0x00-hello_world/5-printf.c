933f676c5fea4be089917eed95d0917faa96fac1
meghamohan <meghamohan1989@gmail.com>
2017-02-08 23:22:52 +0000

    adding task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+* Write a program that prints a string
+*/
+
+/*
+*Description: This program prints a trsing using printf
+* main -main function of the program
+* Returns 0
+*/
+main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
