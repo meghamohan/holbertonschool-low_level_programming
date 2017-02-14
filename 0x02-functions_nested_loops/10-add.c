da71cfac69869fb2149e8e555b1a5dee0bcdc0cf
meghamohan <meghamohan1989@gmail.com>
2017-02-14 23:26:33 +0000

    adding task 9,10,11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* add - a function that adds two numbers.
+* @m: int
+* @n: int
+* Return: an int
+**/
+
+int add(int m, int n)
+{
+	int result;
+
+	result = m + n;
+	return (result);
+}
