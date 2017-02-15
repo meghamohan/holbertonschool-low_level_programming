51d0b6b341035b42ab076d1e5da6c4543303a232
meghamohan <meghamohan1989@gmail.com>
2017-02-15 23:46:47 +0000

    adding task14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,16 +10,22 @@ int main(void)
 
 	n1 = 0;
 	n2 = 1;
+	temp = 0;
 
 	for (n = 0 ; n < 50 ; n++)
 	{
 	temp = n1 + n2;
 	n1 = n2;
 	n2 = temp;
+	printf("%lu", temp);
 	if (n <= 49)
-	printf("%lu,", temp);
+	{
+	printf(", ");
+	}
 	else
-	printf("%lu\n", temp);
+	{
+	printf("\n");
+	}
 	}
 return (0);
 }
