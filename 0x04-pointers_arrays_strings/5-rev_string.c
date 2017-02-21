6c030851c780aa23f8a9385558300b169a146e3e
meghamohan <meghamohan1989@gmail.com>
2017-02-21 20:44:22 +0000

    adding task 5

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,10 @@ void rev_string(char *s)
 {
 	int i, j, k, length;
 	char temp1, temp2;
+
 	for (i = 0 ; s[i] != '\0' ; i++)
 		;
-	length = i -1;
+	length = i - 1;
 	k = 0;
 	for (j = length ; k < length ; j--)
 	{
@@ -20,6 +21,6 @@ void rev_string(char *s)
 		temp1 = s[k];
 		s[k] = temp2;
 		s[j] = temp1;
-		k++; 
+		k++;
 	}
 }
