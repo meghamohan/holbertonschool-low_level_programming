c9908890eb92a730b12c3558b03d64eebd950a5a
meghamohan <meghamohan1989@gmail.com>
2017-02-21 23:38:16 +0000

    adding task8

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0 ; i < (n - 1) ; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		if (i < n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
 	}
-	printf("%d\n", a[n - 1]);
+		printf("\n");
 }
