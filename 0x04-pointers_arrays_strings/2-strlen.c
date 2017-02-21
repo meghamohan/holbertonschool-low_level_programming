f014b29d7e10dc1af9ed98817e8d0dded8d6017e
meghamohan <meghamohan1989@gmail.com>
2017-02-21 18:33:40 +0000

    task2

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* _strlen - to find the length of a string
+* @s: char pointer
+* Return: length of the string
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 1 ; s[i] != '\0' ; i++)
+	{
+	}
+	return (i);
+}
