3ec518f4b14352471c855de9245418887f6d1879
meghamohan <meghamohan1989@gmail.com>
2017-02-23 21:36:24 +0000

    adding tasl3

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _strcmp - compares two strings
+* @dest: string to be compared
+* @src: string to be compared
+* Return: return 0 if strings are same ,neg or postive num otherwise
+**/
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s2 - *s1);
+		s1++;
+		s2++;
+	}
+	return (*s2 - *s1);
+}
