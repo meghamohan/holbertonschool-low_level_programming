192c6dcd6ff1c819ececf5de874cc64137d40d35
meghamohan <meghamohan1989@gmail.com>
2017-02-24 01:09:45 +0000

    corrected betty styles

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -7,9 +7,9 @@
 **/
 void reverse_array(int *a, int n)
 {
-	int temp, i = 0, limit = n-1;
-	
-	while (i < (n/2))
+	int temp, i = 0, limit = n - 1;
+
+	while (i < (n / 2))
 	{
 		temp = a[limit];
 		a[limit] = a[i];
