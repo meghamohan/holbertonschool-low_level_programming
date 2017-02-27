ac12ec15b37d4d77c69664a283bb3a8a57b2d5a3
meghamohan <meghamohan1989@gmail.com>
2017-02-27 21:57:57 +0000

    adding task 1 and 2

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
-*
-*
-*
-*
-*
+* *_memcpy - copies memory
+* @dest: pointer to memory to copy
+* @src: pointer to memory to copy from
+* @n: bytes of memory area
+* Return: pointer to dest
 **/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	
+
 	for (i = 0 ; i < n ; i++)
 	{
 		dest[i] = src[i];
---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+* *_strchr - finds and locates character in a string
+* @s: pointer to string
+* @c: character to search
+* Return: pointer to first occurrence of the c in s, or NULL
+**/
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		{
+			p = &s[i];
+			return (p);
+		}
+	}
+	return (NULL);
+}
