240797f4ea0b202b9090679eb90458c7d13c589a
meghamohan <meghamohan1989@gmail.com>
2017-03-01 21:44:24 +0000

    adding task 1,2,3

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* factorial - calculates the factorial of a given number.
+* @n: given number
+* Return: factorial
+**/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
