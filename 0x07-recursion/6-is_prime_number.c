edb447b5c329a719469ac5b51e0dc1b84737d0ee
meghamohan <meghamohan1989@gmail.com>
2017-03-02 16:31:18 +0000

    adding task6

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -8,12 +8,8 @@ int is_prime_number(int n)
 {
 	if (n == 1 || n < 0)
 		return (0);
-//if number can be divided by 2 then it is not prime
 	if (n % 2 == 0)
-            return (0);
-
-/* if number can be divided by any other number then it is not prime,
-it is incremented by 2 since we checked for even numbers already */
+		return (0);
 	return (check_prime(n, 3));
 }
 /**
@@ -24,12 +20,10 @@ it is incremented by 2 since we checked for even numbers already */
   */
 int check_prime(int x, int y)
 {
-	if(x == y)
+	if (x == y)
 		return (1);
-	if(x % y == 0)
-		return(0);
+	if (x % y == 0)
+		return (0);
 	else
-		return check_prime(x, y + 2);
-	
+		return (check_prime(x, y + 2));
 }
-
