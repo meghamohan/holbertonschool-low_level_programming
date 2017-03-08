ab04277bc47907f72d619ca946f62819e562aa85
meghamohan <meghamohan1989@gmail.com>
2017-03-08 05:05:52 +0000

    adding task4

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* free_grid - frees 2d integer grid previously created by alloc_grid
+* @grid: pointer to array of int
+* @height: number of rows
+* Return: nothing, frees memory
+**/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
