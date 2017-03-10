ca7c4812d154decc9877ad9ad40ec065459d2011
meghamohan <meghamohan1989@gmail.com>
2017-03-10 16:58:53 +0000

    orrection to task3

--- 0x0B-more_malloc_free/3-array_range.c
+++ 0x0B-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	array = malloc((max - min) * sizeof(int));
+	array = malloc((max - min + 1) * sizeof(int));
 	if (array == NULL)
 		return (NULL);
 	i = 0;
