2f4f53b8ca80d61f8013e030a507d392017a814c
meghamohan <meghamohan1989@gmail.com>
2017-03-15 18:35:21 +0000

    adding task0

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function to call a function to print a name
+ *
+ * @name: name to print
+ * @f: function to call to print a name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0E-function_pointers/README.md
@@ -0,0 +1,2 @@
+What are function pointers and how to use them
+What does a function pointer exaclty hold
