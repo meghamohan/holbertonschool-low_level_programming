42aff79d5b49e67fe913478d5fb4d18ad539296e
meghamohan <meghamohan1989@gmail.com>
2017-03-15 18:49:31 +0000

    correction of task0

--- 0x0E-function_pointers/0-print_name.c
+++ 0x0E-function_pointers/0-print_name.c
@@ -2,11 +2,11 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function to call a function to print a name
- *
- * @name: name to print
- * @f: function to call to print a name
- */
+* print_name - function to call a function to print a name
+*
+* @name: name to print
+* @f: function to call to print a name
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	(*f)(name);
