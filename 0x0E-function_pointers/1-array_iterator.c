0d382d7fa1f9e3d17789a45101b7b125a38e42cb
meghamohan <meghamohan1989@gmail.com>
2017-03-15 19:17:37 +0000

    adding task1

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function to call another function against every element in
+ * an array
+ * @array: array to work again
+ * @size: size of array
+ * @action: function to call
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		(*action)(array[i]);
+	}
+}
