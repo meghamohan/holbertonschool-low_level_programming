d34a43a1fae4edd43d1f6508eb5f3f2b359290f0
meghamohan <meghamohan1989@gmail.com>
2017-03-16 23:50:13 +0000

    correction of advanced task

--- 0x0E-function_pointers/100-main_opcodes.c
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -11,24 +11,24 @@
 int main(int argc, char *argv[])
 {
 	int i, bytes;
-	char *pointer;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit (1);
+		exit(1);
 	}
 	bytes = atoi(argv[1]);
 	if (bytes < 0)
 	{
 		printf("Error\n");
-		exit (2);
+		exit(2);
 	}
 	for (i = 0; i < bytes ; i++)
 	{
 		printf("%02hhx", ((char *)main)[i]);
-		if ( i == (bytes - 1))
+		if (i == (bytes - 1))
 			printf("\n");
 		printf(" ");
 	}
+	return (0);
 }
