ef3d5c659741c512fbfdc97849ca4602d17ff8c3
meghamohan <meghamohan1989@gmail.com>
2017-03-16 17:14:54 +0000

    correction

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -21,9 +21,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (i < 6)
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (strcmp(ops[i].op, s) == 0 && *(s+1) == '\0')
 		{
 			return (ops[i].f);
 		}
