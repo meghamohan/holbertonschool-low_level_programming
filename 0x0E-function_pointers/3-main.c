69f034486f7ae50532547588101b84c363d97cfd
meghamohan <meghamohan1989@gmail.com>
2017-03-16 16:44:46 +0000

    correction

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -14,11 +14,16 @@
 int main(int argc, char *argv[])
 {
 	int result;
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		return (100);
 	}
 	result = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", result);
