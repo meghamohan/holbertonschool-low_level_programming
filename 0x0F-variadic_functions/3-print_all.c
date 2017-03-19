084ee72b3bdda181a350d3e365b433aab046dcec
meghamohan <meghamohan1989@gmail.com>
2017-03-19 05:54:20 +0000

    task3 fixes

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -43,7 +43,7 @@ void print_all(const char * const format, ...)
 				printf("(nil)");
 				break;
 		}
-		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' 
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
 			|| format[i] == 's') && format[i + 1] != '\0')
 			printf(", ");
 		i++;
