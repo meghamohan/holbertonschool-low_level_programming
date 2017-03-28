aa62eda2f610d4c2b0584fecf7247d55b57634a8
meghamohan <meghamohan1989@gmail.com>
2017-03-28 16:47:14 +0000

    adding task5

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a linked list
+ * @head: pointer to linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head !=  NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+}
