38335ab566101cbe6663b13d8c6ea1db20982cd7
meghamohan <meghamohan1989@gmail.com>
2017-03-28 05:16:02 +0000

    adding task0

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t list
+ * @h: pointer to linked list
+ * Return: number of elements in the linkedlist
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	unsigned int n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
