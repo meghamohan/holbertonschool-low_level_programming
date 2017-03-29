85b53ff54f668ab0ee71ef638dd5f137d223cc68
meghamohan <meghamohan1989@gmail.com>
2017-03-29 19:33:35 +0000

    null check added

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (idx == 0)
 	{
+		newNode->n = n;
 		newNode->next = *head;
 		*head = newNode;
 		return (newNode);
