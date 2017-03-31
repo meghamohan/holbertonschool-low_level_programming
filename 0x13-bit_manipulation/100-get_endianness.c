3c12dfc71365c3ed33fb6e44f13c197e7eb5dca8
meghamohan <meghamohan1989@gmail.com>
2017-03-31 22:06:15 +0000

    adding advtask1

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/*
+ * get_endianness - checks the endianness of the system
+ * Return: 1 if little indian, 0 if big endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i;
+	char *c;
+
+	i = 1;
+	c = (char *)&i;
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
