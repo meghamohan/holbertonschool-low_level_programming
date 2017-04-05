ec46441f6495e59c6f0db6fda5e55717a595d440
meghamohan <meghamohan1989@gmail.com>
2017-04-05 17:13:06 +0000

    betty fix

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -19,28 +19,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
-
 	file = open(filename, O_RDWR);
 	if (file == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-
 	readReturn = read(file, buffer, letters);
 	if (readReturn == -1)
 		return (0);
-
 	writeReturn = write(STDOUT_FILENO, buffer, readReturn);
 	if (writeReturn == -1)
 		return (0);
-
 	closeReturn = close(file);
 	if (closeReturn == -1)
 		return (0);
-
 	free(buffer);
-	
 	return (readReturn);
 }
