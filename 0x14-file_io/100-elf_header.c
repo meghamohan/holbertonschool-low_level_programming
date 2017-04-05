425544b2f4f62f6781cdcb74cb7bb81108235482
meghamohan <meghamohan1989@gmail.com>
2017-04-05 21:57:27 +0000

    adding empty file

---
+++ 0x14-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - reads elf header and prints it out.
+ *
+ * @argc: number of arguments
+ * @argv: arguments that are passed
+ * Return: Returns 1 on success, 98 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
