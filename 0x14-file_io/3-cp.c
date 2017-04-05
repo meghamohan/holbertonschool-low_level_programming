1bcdd0d320389c4ca4e9dd34642d60583b44e2b4
meghamohan <meghamohan1989@gmail.com>
2017-04-05 20:49:29 +0000

    betty fix and adding helper functions

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,10 +1,44 @@
 #include "holberton.h"
 #define BUFFER 1024
 
+/**
+ * argError - prints error if number of args is wrong
+ * Return: none
+ */
+void argError(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+/**
+ * readError - prints error if there is error reading from file
+ * @file: pointer to a file
+ * Return: none
+ */
 void readError(char *file)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
 	exit(98);
+}
+/**
+ * writeError - prints error text if there is error writing to a file
+ * @file: pointer to a file
+ * Return: none
+ */
+void writeError(char *file)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	exit(99);
+}
+/**
+ * closeError - prints error if you encounter error while closing file
+ * @fd: file descriptor
+ * Return: none
+ */
+void closeError(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
 }
 
 /**
@@ -21,11 +55,7 @@ int main(int argc, char **argv)
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+		argError();
 	source = open(argv[1], O_RDONLY);
 	if (source == -1)
 		readError(argv[1]);
@@ -40,21 +70,16 @@ int main(int argc, char **argv)
 	{
 		writeReturn = write(dest, &buff, readReturn);
 		if (writeReturn == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			writeError(argv[2]);
 		readReturn = read(source, &buff, BUFFER);
 		if (readReturn == -1)
 			readError(argv[1]);
 	}
 	closeReturn = close(source);
 	if (closeReturn == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source), exit(100);
+		closeError(source);
 	closeReturn = close(dest);
 	if (closeReturn == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest), exit(100);
-
+		closeError(dest);
 	return (0);
 }
-
