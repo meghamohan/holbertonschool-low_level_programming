30dacd6ba2beb9a4a135de365e6ace999d886875
meghamohan <meghamohan1989@gmail.com>
2017-05-05 20:30:57 +0000

    adding tasks4 and 5

---
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - returns the nth node of 
+* a linkedlist
+* @head: the pointer to head of a linkedlist
+* @index: index of the node to be returned
+* Return: retruns the node in the index position
+**/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+	dlistint_t *temp;
+
+	count = 0;
+	if (head == NULL)
+		return (NULL);
+
+	temp = head;
+	while (temp)
+	{
+		if (index == count)
+			return (temp);
+		count++;
+		temp = temp->next;
+	}
+	return (NULL);
+}
