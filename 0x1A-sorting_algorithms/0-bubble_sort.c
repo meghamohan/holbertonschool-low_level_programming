1b874896322d8b75ef89b177ab4bc5eeea251eed
meghamohan <meghamohan1989@gmail.com>
2017-06-16 21:27:42 +0000

    adding task0

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * swap - swap adjescent array elements
+ * @a: points to first array element
+ * @b: points to second array element
+ * Return - nothing
+**/
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - sort array of elements using bubble sort algo
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return - nothing
+**/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	char sortingDone;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		sortingDone = 1;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				sortingDone = 0;
+				print_array(array, size);
+			}
+		}
+		/**
+		* if swapping is not done by inner for loop,
+		* then break out of it
+		* as it means that the array is already sorted
+		*/
+		if (sortingDone == 1)
+			break;
+	}
+}
---
+++ 0x1A-sorting_algorithms/sort.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
