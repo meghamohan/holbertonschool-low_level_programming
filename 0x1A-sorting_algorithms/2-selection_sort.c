fe8a38bf7db1bc160e4fdd7f933a81a3edb945b4
clturner <clturner23@gmail.com>
2017-06-17 01:25:08 +0000

    selection sort

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * selection_sort - sorts a list of integers in ascending order
+ * @array: array of ints
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+        unsigned int i,k,j, index;
+
+        for (i = 0; i < size; i++)
+        {
+                index = i; /* set index to i in loop */
+                for (k = i; k < size; k++)
+                {
+                        /* If first num greater than second num*/
+                        if (array[index] > array[k] )
+                        {
+                                /* set the index to equal index
+                                   of 1st out of sequence numbers */
+                                index = k;
+                        }
+                }
+                if ( index != i )
+                {
+                        /* swap intgers */
+                        j = array[i];
+                        array[i] = array[index];
+                        array[index] = j;
+                        print_array(array, size);
+                }
+        }
+}
