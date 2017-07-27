e9ec99b7f0be0b017917cb8c325f27e0540f2a13
meghamohan <meghamohan1989@gmail.com>
2017-07-27 19:27:34 +0000

    finally it works! made code concised and changed the logic

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -1,5 +1,8 @@
 #include "binary_trees.h"
 #include "14-binary_tree_balance.c"
+#include "13-binary_tree_nodes.c"
+#include "15-binary_tree_is_full.c"
+
 /**
 * binary_tree_is_perfect - checks if a binary tree is perfect,
 * ie balanced and ends with leaves
@@ -8,19 +11,22 @@
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int nodeCount = 0;
+
 	if (!tree)
 		return (0);
 	/**
 	* for a binary tree to be perfect
 	* it should be balanced and also
 	* each node should have 2 leaves
-	ie call the functions balance and perfect
+	* and number of nodes should be odd
+	* ie parent + child(even)
 	*/
+	nodeCount = binary_tree_nodes(tree);
 	if (!tree->left && !tree->right)
 		return (1);
-	if (!binary_tree_balance(tree))
-		return (0);
-	if (tree->left && tree->right)
-		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-	return (1);
+	if ((nodeCount % 2) && !binary_tree_balance(tree) &&
+		 binary_tree_is_full(tree))
+		return (1);
+	return (0);
 }
